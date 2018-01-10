f78d6a274b96e9a5de5f8a65b3e99632451d228b
Szymon Harabasz <szymon.harabasz@gmail.com>
2018-01-10 08:48:20 +0100

    Functional test now checks the capability to add a list item.

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,8 +13,28 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
+        
         self.assertIn('Listy', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1')
+        self.assertIn('Listy', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Wpisz rzecz do zrobienia'
+                )
+
+        inputbox.send_keys('Kupic pawie piora')
+        inputbox.send_keys(Keys.ENTER)
+
+
         self.fail('Zakończenie testu!')
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: Kupic pawie piora' for row in rows)
+                )
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
